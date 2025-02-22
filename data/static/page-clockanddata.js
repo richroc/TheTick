33204c601f51f2cbd16f8ee9ec19ae248dac988e
Jakub Kramarz <jakub@hackerspace-krk.pl>
2025-02-22 12:43:42 +0100

    cleanup license files

--- data/static/page-clockanddata.js
+++ data/static/page-clockanddata.js
@@ -1,3 +1,18 @@
+// Copyright (C) 2024, 2025 Jakub "lenwe" Kramarz
+// This file is part of The Tick.
+// 
+// The Tick is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// The Tick is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+
 function decode_aba_track_ii(card_data){
   function get_num(bcd){
     return String(8 * bcd[3] + 4 * bcd[2] + 2 * bcd[1] + 1 * bcd[0]);
