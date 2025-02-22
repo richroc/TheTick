cc0877660612749f28128f21dcf250ffe967ecdf
Jakub Kramarz <jakub@hackerspace-krk.pl>
2025-02-22 12:49:33 +0100

    cleanup clock&data code

--- src/tick_clockanddata_reader.h
+++ src/tick_clockanddata_reader.h
@@ -49,42 +49,10 @@ void clockanddata_trigger(void) {
   clockanddata_last_received=now;
 }
 
-// static void wiegand_fix_reader1_string(void)
-// {
-//   byte loose_bits = reader1_count % 4;
-//   if (loose_bits > 0)
-//   {
-//     byte moving_bits = 4 - loose_bits;
-//     byte moving_mask = pow(2, moving_bits) - 1;
-//     // DBG_OUTPUT_PORT.println("lb: " + String(loose_bits) + " mb: " + String(moving_bits) + " mm: " + String(moving_mask, HEX));
-//     char c = hex_to_byte(reader1_string.charAt(0));
-//     for (unsigned long i = 0; i < reader1_string.length(); i++)
-//     {
-//       reader1_string.setCharAt(i, c2h(c >> moving_bits));
-//       c &= moving_mask;
-//       c = (c << 4) | hex_to_byte(reader1_string.charAt(i + 1));
-//       // DBG_OUTPUT_PORT.println("c: " + String(c, HEX) + " i: " + String(reader1_string.charAt(i)));
-//     }
-//     reader1_string += String((c >> moving_bits) | reader1_byte, HEX);
-//   }
-//   reader1_string += ":" + String(reader1_count);
-// }
-
 static void clockanddata_reset(void) {
   clockanddata_string.clear();
   clockanddata_last_received = 0;
 }
-
-
-// static void wiegand_transmit_assert(byte bit) {
-//   byte pin = bit ? wiegand_pin_d1 : wiegand_pin_d0;
-//   digitalWrite(pin, LOW);
-//   pinMode(pin, OUTPUT);
-//   delayMicroseconds(wiegand_pulse_width);
-//   pinMode(pin, INPUT);
-//   digitalWrite(pin, HIGH);
-//   delayMicroseconds(wiegand_pulse_gap);
-// }
 
 void clockanddata_transmit_id(String sendValue, unsigned long bitcount)
 {
@@ -122,10 +90,6 @@ void clockanddata_loop(void){
     card_read_handler(received_card);
   }
 }
-
-#else
-
-// #error "Clockanddata support is currently required"
 
 #endif
 #endif
